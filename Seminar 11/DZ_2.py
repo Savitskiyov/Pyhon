a4bb9da8b0f84d6e7389da57cc7068bdf891c722
«savitskiyov» <savitskiyov@gmail.com>
2022-11-15 10:03:02 +0300

    Домашнее задание по 11 семинару

---
+++ Seminar 11/DZ_2.py
@@ -0,0 +1,112 @@
+from sympy import symbols, sin, cos
+from sympy.plotting import plot
+from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
+import numpy
+
+# График функций, при помощи библиотеки matplotlib:
+x = [x for x in range(-30, 30)]
+y = [(-12 * x ** 4 * sin(cos(x)) - 18 * x ** 3 + 5 * x ** 2 + 10 * x - 30) for
+     x in range(-30, 30)]
+# y = [y for y in range(-30, 30)]
+plt.plot(x, y)
+plt.grid()
+plt.show()
+
+# График функций, при помощи библиотеки sympy:
+# В диапазоне -300...300
+x = symbols('x')
+plot(-12 * x ** 4 * sin(cos(x)) - 18 * x ** 3 + 5 * x ** 2 + 10 * x - 30,
+     (x, -300, 300))
+
+# В диапазоне -10...10
+x = symbols('x')
+plot(-12 * x ** 4 * sin(cos(x)) - 18 * x ** 3 + 5 * x ** 2 + 10 * x - 30,
+     (x, -10, 10))
+
+
+# Определить корни
+def f(x):
+    return -12 * x ** 4 * numpy.sin(
+        numpy.cos(x)) - 18 * x ** 3 + 5 * x ** 2 + 10 * x - 30
+
+
+# Это тригометрическая функция, имеющая бесконечное количество корней
+# можно определить корни только на занном интервале
+# Запросим их у пользователя:
+# funcrange = list(
+#     map(int, input('Задайте интервал функций, через пробел: ').split()))
+funcrange = [-10, 10]
+leftnum = min(funcrange)
+rightnum = max(funcrange)
+
+
+# Задайте интервал функций, через пробел: -5 5
+
+
+def solution():
+    global leftnum, rightnum
+    temp = leftnum
+    rightnum = rightnum
+    roots = []
+    interval = []
+
+    while temp < rightnum:
+        if f(temp) >= 0 and f(temp + 1) <= 0:
+            w = fsolve(f, temp)
+            roots.append(*w)
+        if f(temp) <= 0 and f(temp + 1) >= 0:
+            w = fsolve(f, temp)
+            roots.append(*w)
+        if f(temp) > f(temp + 1) < f(temp + 2):
+            interval.append(temp + 1)
+        temp += 1
+    roots = [round(i, 2) for i in roots]
+    print(f'Корни уравнения для заданного интервала: {roots}')
+    return roots
+
+
+# Определить промежутки, на которых f>0 Рё f<0:
+def func_interval(left, right):
+    array = []
+    temp = left
+    while left < right:
+        array.append([f(left), left])
+        left += 0.1
+    if array[0][0] > 0:
+        print(f'f > 0 в промежутке {temp, right}')
+        return max(array)
+    else:
+        print(f'f < 0 в промежутке {temp, right}')
+        return min(array)
+
+
+# вычисляем координаты вершины функции на заданном интервале:
+def maxima_and_minima():
+    roots = solution()
+
+    if len(roots) < 2:
+        print('На заданном интервале нет вершин')
+    else:
+        top = []
+        for i in range(len(roots) - 1):
+            top.append(func_interval(roots[i], roots[i + 1]))
+        for j in top:
+            j = [round(i, 2) for i in j]
+            print(f'Координаты вершин функции: [{j[1]}, {j[0]}]')
+        if len(top) < 2:
+            print('error')
+        else:
+            for i in range(len(top) - 1):
+                if top[i][0] > top[i + 1][0]:
+                    print('Функция убывает')
+                else:
+                    print('Фунция возрастает')
+
+
+maxima_and_minima()
+# Корни уравнения для заданного интервала: [-1.34, 2.27, 4.38]
+# f > 0 в промежутке (-1.34, 2.27)
+# f < 0 в промежутке (2.27, 4.38)
+# Координаты вершин функций: [-1.34, 0.11]
+# Координаты вершин функций: [2.27, -0.88]
---
+++ Seminar 11/readme.md
@@ -0,0 +1,46 @@
+https://colab.research.google.com/
+
+
+1. pip install matplotlib
+2. pip install numpy
+3. pip install sympy
+4. pip install scipy
+5. pip install scipy
+
+6. https://pypi.org/project/matplotlib/
+7. https://pypi.org/project/numpy/
+8. https://pypi.org/project/sympy/
+9. https://pypi.org/project/scipy/
+10. https://pypi.org/project/scipy/
+
+
+1. https://matplotlib.org/stable/users/installing/index.html
+2. https://matplotlib.org/stable/tutorials/introductory/pyplot.html
+3. https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.plot.html
+4. https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.show.html
+
+5. https://numpy.org/install/
+6. https://numpy.org/doc/stable/reference/generated/numpy.sin.html
+7. https://numpy.org/doc/stable/reference/generated/numpy.roots.html
+8. https://numpy.org/doc/stable/reference/generated/numpy.diff.html
+
+9. https://docs.sympy.org/latest/install.html
+10. https://docs.sympy.org/latest/tutorials/intro-tutorial/basic_operations.html
+11. https://docs.sympy.org/latest/tutorials/intro-tutorial/manipulation.html
+12. https://docs.sympy.org/latest/tutorials/intro-tutorial/solvers.html
+13. https://docs.sympy.org/latest/guides/solving/solve-equation-algebraically.html
+14. https://docs.sympy.org/latest/modules/functions/elementary.html#trigonometric-functions
+15. https://docs.sympy.org/latest/modules/plotting.html
+16. https://docs.sympy.org/latest/modules/polys/basics.html
+17. https://docs.sympy.org/latest/modules/core.html
+
+18. https://www.tutorialspoint.com/sympy/sympy_symbols.htm
+
+19. https://scipy.org/install/
+20. https://docs.scipy.org/doc/scipy/getting_started.html
+21. https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.fsolve.html
+22. https://docs.scipy.org/doc/scipy/tutorial/index.html
+23. https://docs.scipy.org/doc/scipy/tutorial/optimize.html
+
+
+https://www.youtube.com/watch?v=v4R6K4RxADE
