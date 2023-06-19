24043814d17822480c21f1e50f4b7efe7de05f22
Savitskiyov <108422439+Savitskiyov@users.noreply.github.com>
2023-06-19 18:33:45 +0300

    Update DZ4.py

--- Python_sem_4/DZ4.py
+++ Python_sem_4/DZ4.py
@@ -7,9 +7,14 @@ num = [52, 19, 12, 7323, 8, 15, 1, 2, 3, 4, 5]
 index_1 = int(input('Введите первый индекс: '))
 index_2 = int(input('Введите второй индекс: '))
 def my_func(mas , first, second):
-    index = 0 if first < 0 else first
-    s = sum(mas[index:second+1])  # взять сумму между индексами
-    return s
+    if index_1 <= index_2:
+        index = 0 if first < 0 else first
+        s = sum(mas[index:second+1])  # взять сумму между индексами
+        return s
+    else:
+        index = 0 if first < 0 else first
+        s = sum(mas[second:index+1])  # взять сумму между индексами
+        return s
 
 
 print(my_func(num, index_1, index_2))
